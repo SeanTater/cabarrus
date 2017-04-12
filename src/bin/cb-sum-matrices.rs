485d555d8bc8dcc7120f24db85dd2311f98647e0
Sean Gallagher <stgallag@gmail.com>
2017-04-12 18:51:43 -0400

    A little more debug info still

--- src/bin/cb-sum-matrices.rs
+++ src/bin/cb-sum-matrices.rs
@@ -19,7 +19,7 @@ pub fn inner_main() -> Result<()> {
     let args = app_from_crate!()
         .arg_from_usage("<size> 'how many workers there are'")
         .arg_from_usage("<rank> 'which worker am I, starting from 0'")
-        .arg_from_usage("<addends>.. 'files containin matrices to add'")
+        .arg_from_usage("<addends>.. 'files containing matrices to add'")
         .arg_from_usage("<output> 'file in which to store the resulting matrix'")
         .get_matches();
     
@@ -29,13 +29,14 @@ pub fn inner_main() -> Result<()> {
     let size = value_t!(args, "size", usize).unwrap_or_else(|e| e.exit());
     let rank = value_t!(args, "rank", usize).unwrap_or_else(|e| e.exit());
     let chunksize = (mats.len() + size - 1) / size;
-    info!("{} files, {} workers, chunks of {}", mats.len(), size, chunksize);
+    info!("{} files, {} workers (I'm #{}), chunks of {}", mats.len(), size, rank, chunksize);
     
     let mut accum = None;
     for matname in mats.into_iter().skip(rank*chunksize).take(chunksize) {
-        info!("Reading {}..", matname);
         let ref matfile = cabarrus::numpy::open_matrix_mmap(matname)?;
         let ref mat = cabarrus::numpy::read_matrix_mmap(matfile)?;
+        
+        info!("Reading {} ({} GB) ..", matname, matname.len() >> 27);
         accum = Some(match accum {
             Some(mut acc) => {acc += mat; acc}
             None => mat.to_owned()

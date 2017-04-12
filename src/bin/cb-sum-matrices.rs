bed1f86d245058e909bc1b1baf126a740bc85c64
Sean Gallagher <stgallag@gmail.com>
2017-04-12 14:36:21 -0400

    even better with the code

---
+++ src/bin/cb-sum-matrices.rs
@@ -0,0 +1,47 @@
+/// MPI-based method for summing many arrays
+
+// Argument parsing
+#[macro_use] extern crate clap;
+// logging
+#[macro_use] extern crate log;
+extern crate ndarray;
+extern crate env_logger;
+// lastly, this library
+extern crate cabarrus;
+use cabarrus::errors::*;
+use std::cmp::min;
+use ndarray as nd;
+use ndarray::prelude::*;
+
+pub fn main() {
+    // Main can't return a Result, and the ? operator needs the enclosing function to return Result
+    inner_main().expect("Could not recover. Exiting.");
+}
+pub fn inner_main() -> Result<()> {
+    env_logger::init().unwrap();
+    let args = app_from_crate!()
+        .arg_from_usage("<size> 'how many workers there are' ")
+        .arg_from_usage("<rank> 'which worker am I, starting from 0' ")
+        .arg_from_usage("<addends>.. 'files contains matrices to add'")
+        .arg_from_usage("<output> 'file in which to store the resulting matrix'")
+        .get_matches();
+    
+    let mats = args.values_of("addends").unwrap();
+    let size = value_t!(args, "size", usize).unwrap_or_else(|e| e.exit());
+    let rank = value_t!(args, "rank", usize).unwrap_or_else(|e| e.exit());
+    let chunksize = (mats.len() + size - 1) / size;
+    
+    let mut accum = None;
+    for matname in mats.skip(rank*chunksize).take(chunksize) {
+        let ref matfile = cabarrus::numpy::open_matrix_mmap(matname)?;
+        let ref mat = cabarrus::numpy::read_matrix_mmap(matfile)?;
+        accum = Some(match accum {
+            Some(mut acc) => {acc += mat; acc}
+            None => mat * 1.0
+        });
+    }
+    if let Some(ref acc) = accum {
+        cabarrus::numpy::write_matrix(args.value_of("output").unwrap(), acc)?;
+    }
+    Ok(())
+}
